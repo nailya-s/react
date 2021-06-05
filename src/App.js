ebb7ab779b0477810488e47b8ede3c038566fc3e
Nailya Salekhova <salehova.n@gmail.com>
2021-06-05 14:03:04 +0600

    mid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+import {BrowserRouter, Route} from "react-router-dom";
 
-function App() {
+
+
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+<div className= 'app-wrapper'>
+        <Header/>        
+        <Navbar state={props.state.sideBar} />
+        <div className= 'app-wrapper-content'>
+        <Route path = '/music' component = {Music}/ >
+        <Route path = '/news' component = {News}/ >
+        <Route path = '/settings' component = {Settings}/ >
+        <Route path = '/dialogs' render = {() => <Dialogs store={props.store}/> } / >
+        <Route path = '/profile' render = {() => <Profile state={props.state.profilePage} dispatch ={props.dispatch}/> }/>
+        </div>
+        
+      </div>);
 }
+
 
 export default App;
